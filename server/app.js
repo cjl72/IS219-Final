96b464dcacabe2c3e403969d8ba7e7200f498a0c
cjl72 <cjl72@njit.edu>
2021-05-10 15:28:55 -0400

    Task: Added passport configuration

--- server/app.js
+++ server/app.js
@@ -39,6 +39,34 @@ if (app.get("env") === "production") {
   session.cookie.secure = true;
 }
 
+const strategy = new Auth0Strategy(
+    {
+      domain: process.env.AUTH0_DOMAIN,
+      clientID: process.env.AUTH0_CLIENT_ID,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET,
+      callbackURL:
+          process.env.AUTH0_CALLBACK_URL || 'http://localhost:8000/callback',
+    },
+    ((accessToken, refreshToken, extraParams, profile, done) =>
+            // accessToken is the token to call Auth0 API (not needed in the most cases)
+            // extraParams.id_token has the JSON Web Token
+            // profile has all the information from the user
+            done(null, profile)
+    ),
+);
+
+passport.use(strategy);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
 // define a root route
 
 app.use(express.static(path.join(__dirname, "public")));
