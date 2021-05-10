b86b030a29f9e03db5d1b71270dfd20159daa84b
cjl72 <cjl72@njit.edu>
2021-05-10 15:33:06 -0400

    Task: Added callback endpoint in auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -15,4 +15,17 @@ router.get(
     }
 );
 
+router.get('/callback', function (req, res, next) {
+    passport.authenticate('auth0', function (err, user, info) {
+        if (err) { return next(err); }
+        if (!user) { return res.redirect('/login'); }
+        req.logIn(user, function (err) {
+            if (err) { return next(err); }
+            const returnTo = req.session.returnTo;
+            delete req.session.returnTo;
+            res.redirect(returnTo || '/');
+        });
+    })(req, res, next);
+});
+
 
